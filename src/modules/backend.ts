13d0a62fec927f28f790f6bb2976ec5e26ad4430
artur <brugeman.artur@gmail.com>
2024-02-12 12:42:32 +0300

    Fix bad validity checks on confirm modal

--- src/modules/backend.ts
+++ src/modules/backend.ts
@@ -745,6 +745,7 @@ export class NoauthBackend {
         // OAuth flow
         const confirmMethod = method === 'connect' ? 'confirm-connect' : 'confirm-event'
         const authUrl = `${self.swg.location.origin}/key/${npub}?${confirmMethod}=true&appNpub=${appNpub}&reqId=${id}&popup=true`
+//        const authUrl = `${self.swg.location.origin}/key/${npub}?popup=true`
         console.log('sending authUrl', authUrl, 'for', req)
         // NOTE: if you set 'Update on reload' in the Chrome SW console
         // then this message will cause a new tab opened by the peer,
