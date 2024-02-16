04c425c32cbdc21b949d7f12919ebc8113d32acb
Bekbolsun <bekbolsun.azamatov.1579.7@gmail.com>
2024-02-16 14:20:51 +0600

    show appNpub in apps list & in app details page

--- src/components/Modal/ModalInitial/ModalInitial.tsx
+++ src/components/Modal/ModalInitial/ModalInitial.tsx
@@ -1,30 +1,13 @@
-import React, { useEffect, useState } from 'react'
 import { useModalSearchParams } from '@/hooks/useModalSearchParams'
 import { Button } from '@/shared/Button/Button'
 import { Modal } from '@/shared/Modal/Modal'
 import { MODAL_PARAMS_KEYS } from '@/types/modal'
-import { Fade, Stack } from '@mui/material'
-import { AppLink } from '@/shared/AppLink/AppLink'
+import { Stack } from '@mui/material'
 
 export const ModalInitial = () => {
   const { getModalOpened, createHandleCloseReplace, handleOpen } = useModalSearchParams()
   const isModalOpened = getModalOpened(MODAL_PARAMS_KEYS.INITIAL)
-
   const handleCloseModal = createHandleCloseReplace(MODAL_PARAMS_KEYS.INITIAL)
-
-  const [showAdvancedContent, setShowAdvancedContent] = useState(false)
-
-  const handleShowAdvanced = () => {
-    setShowAdvancedContent(true)
-  }
-
-  useEffect(() => {
-    return () => {
-      if (isModalOpened) {
-        setShowAdvancedContent(false)
-      }
-    }
-  }, [isModalOpened])
 
   return (
     <Modal open={isModalOpened} onClose={handleCloseModal}>
@@ -32,13 +15,6 @@ export const ModalInitial = () => {
         <Button onClick={() => handleOpen(MODAL_PARAMS_KEYS.SIGN_UP)}>Sign up</Button>
         <Button onClick={() => handleOpen(MODAL_PARAMS_KEYS.LOGIN)}>Login</Button>
         <Button onClick={() => handleOpen(MODAL_PARAMS_KEYS.IMPORT_KEYS)}>Import key</Button>
-        {/* <AppLink title="Advanced" alignSelf={'center'} onClick={handleShowAdvanced} />
-
-        {showAdvancedContent && (
-          <Fade in>
-            <Button onClick={() => handleOpen(MODAL_PARAMS_KEYS.IMPORT_KEYS)}>Import key</Button>
-          </Fade>
-        )} */}
       </Stack>
     </Modal>
   )
--- src/pages/KeyPage/utils.ts
+++ src/pages/KeyPage/utils.ts
@@ -1,6 +1,7 @@
 import { db } from '@/modules/db'
 
-export const checkNpubSyncQuerier = (npub: string) => async () => {
+export const checkNpubSyncQuerier = (npub: string, onResolve: () => void) => async () => {
   const count = await db.syncHistory.where('npub').equals(npub).count()
+  if (!count) onResolve()
   return count > 0
 }
