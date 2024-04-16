ffb67eb946a41711b0fd14473ecbd519c03ab248
Bekbolsun <bekbolsun.azamatov.1579.7@gmail.com>
2024-04-16 19:47:37 +0600

    remove device info

--- src/components/Modal/ModalConfirmEvent/ModalConfirmEvent.tsx
+++ src/components/Modal/ModalConfirmEvent/ModalConfirmEvent.tsx
@@ -1,7 +1,7 @@
 import { useModalSearchParams } from '@/hooks/useModalSearchParams'
 import { Modal } from '@/shared/Modal/Modal'
 import { MODAL_PARAMS_KEYS } from '@/types/modal'
-import { getAppDevice, getAppIconTitle, getDomainPort, getReqActionName, getShortenNpub } from '@/utils/helpers/helpers'
+import { getAppIconTitle, getDomainPort, getReqActionName, getShortenNpub } from '@/utils/helpers/helpers'
 import { Box, FormControlLabel, Stack, Typography } from '@mui/material'
 import { useParams, useSearchParams } from 'react-router-dom'
 import { useAppSelector } from '@/store/hooks/redux'
@@ -17,7 +17,6 @@ import { LoadingSpinner } from '@/shared/LoadingSpinner/LoadingSpinner'
 import { SectionTitle } from '@/shared/SectionTitle/SectionTitle'
 import { IconApp } from '@/shared/IconApp/IconApp'
 import { useProfile } from '@/hooks/useProfile'
-import { DeviceInfo } from '@/components/DeviceInfo/DeviceInfo'
 
 enum ACTION_TYPE {
   ALWAYS = 'ALWAYS',
@@ -68,7 +67,7 @@ export const ModalConfirmEvent: FC = () => {
   })
 
   const triggerApp = apps.find((app) => app.appNpub === appNpub)
-  const { name, url = '', icon = '', subNpub = '', userAgent = '' } = triggerApp || {}
+  const { name, url = '', icon = '', subNpub = '' } = triggerApp || {}
   const appDomain = getDomainPort(url)
   const shortAppNpub = getShortenNpub(appNpub)
   const appName = name || appDomain || shortAppNpub
@@ -77,7 +76,6 @@ export const ModalConfirmEvent: FC = () => {
   const isAppNameExists = !!name || !!appDomain
   const { userAvatar, userName } = useProfile(subNpub || '')
   const subNpubName = userName || getShortenNpub(subNpub)
-  const appDevice = getAppDevice(userAgent)
 
   useEffect(() => {
     // reset
@@ -183,8 +181,9 @@ export const ModalConfirmEvent: FC = () => {
                 {shortAppNpub}
               </Typography>
             )}
-
-            {appDevice && <DeviceInfo info={appDevice} />}
+            <Typography variant="body2" color={'GrayText'} noWrap>
+              New app would like to connect
+            </Typography>
           </Box>
         </StyledHeadingContainer>
 
