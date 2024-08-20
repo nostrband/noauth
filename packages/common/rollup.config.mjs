2f64a51cc0ad73d0e680e0a2c0c3735488121300
Bekbolsun <bekbolsun.azamatov.1579.7@gmail.com>
2024-08-20 19:17:59 +0600

    change entrypoint for each build in common

--- packages/common/rollup.config.mjs
+++ packages/common/rollup.config.mjs
@@ -6,51 +6,6 @@ import terser from '@rollup/plugin-terser'
 import json from '@rollup/plugin-json'
 import inject from '@rollup/plugin-inject'
 import dotenv from 'rollup-plugin-dotenv'
-import conditional from 'rollup-plugin-conditional'
-
-// export default {
-//   input: 'index.ts',
-//   output: [
-//     {
-//       file: 'dist/index.js',
-//       format: 'esm',
-//       sourcemap: true,
-//       inlineDynamicImports: true,
-//     },
-//   ],
-//   plugins: [
-//     commonjs(),
-//     json(),
-//     dotenv(),
-//     alias({
-//       entries: [
-//         { find: 'assert', replacement: 'assert' },
-//         { find: 'crypto', replacement: 'crypto-browserify' },
-//         { find: 'https', replacement: 'https-browserify' },
-//         { find: 'os', replacement: 'os-browserify' },
-//         { find: 'stream', replacement: 'stream-browserify' },
-//         { find: 'http', replacement: 'stream-http' },
-//         { find: 'url', replacement: 'url' },
-//       ],
-//     }),
-//     resolve({
-//       browser: true,
-//       preferBuiltins: false,
-//     }),
-//     typescript({
-//       tsconfig: 'tsconfig.json',
-//     }),
-//     inject({
-//       process: 'process/browser',
-//       Buffer: ['buffer', 'Buffer'],
-//     }),
-//     terser({
-//       compress: {
-//         toplevel: true,
-//       },
-//     }),
-//   ],
-// }
 
 export default [
   {
@@ -91,24 +46,6 @@ export default [
         compress: {
           toplevel: true,
         },
-      }),
-    ],
-  },
-  {
-    input: 'index.ts',
-    output: {
-      file: 'dist/node/index.js',
-      format: 'cjs',
-      sourcemap: false,
-      inlineDynamicImports: true,
-    },
-    plugins: [
-      resolve({ browser: false, preferBuiltins: true }),
-      commonjs(),
-      dotenv(),
-      terser(),
-      typescript({
-        tsconfig: 'tsconfig.json',
       }),
     ],
   },
