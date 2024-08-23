6090c15f7cb0e80946d9f27e3996ce9e01a7b822
Bekbolsun <bekbolsun.azamatov.1579.7@gmail.com>
2024-08-23 18:29:35 +0600

    handle connect npub in nostr connect modal

--- packages/client/src/components/Modal/ModalNostrConnect/ModalNostrConnect.tsx
+++ packages/client/src/components/Modal/ModalNostrConnect/ModalNostrConnect.tsx
@@ -1,4 +1,4 @@
-import { Navigate, useParams, useSearchParams } from 'react-router-dom'
+import { Navigate, useNavigate, useParams, useSearchParams } from 'react-router-dom'
 import { Modal } from '@/shared/Modal/Modal'
 import { Box, Stack, Typography } from '@mui/material'
 import { IconApp } from '@/shared/IconApp/IconApp'
@@ -9,12 +9,16 @@ import { Keys } from './components/Keys'
 import { getDomainPort } from '@/utils/helpers/helpers'
 import { useEnqueueSnackbar } from '@/hooks/useEnqueueSnackbar'
 import { client } from '@/modules/client'
+import { useState } from 'react'
 
 export const ModalNostrConnect = () => {
+  const navigate = useNavigate()
   const notify = useEnqueueSnackbar()
   const [searchParams] = useSearchParams()
   const { pubkey = '' } = useParams()
   console.log({ pubkey })
+
+  const [isLoading, setIsLoading] = useState(false)
 
   const keys = useAppSelector(selectKeys)
 
@@ -36,23 +40,27 @@ export const ModalNostrConnect = () => {
   }
 
   const connect = async (npub: string) => {
+    setIsLoading(true)
     try {
       const nostrconnect = `nostrconnect://${pubkey}?${searchParams.toString()}`
       const requestId = await client.nostrConnect(npub, nostrconnect)
-      console.log('requestId', requestId)
-      if (requestId) {
+      setIsLoading(false)
 
-      } else {
-        
-      }
+      console.log('requestId', requestId)
+      if (!requestId) return notify('Connected!', 'success')
+      return navigate(`/key/${npub}?confirm-connect=true&reqId=${requestId}`)
     } catch (e) {
-      console.log(`Error: ${e}`)
       notify('Error: ' + e, 'error')
+      setIsLoading(false)
     }
   }
 
+  const handleClose = () => {
+    navigate('/home')
+  }
+
   return (
-    <Modal title="Connect to app" open withCloseButton={false}>
+    <Modal title="Connect to app" open onClose={handleClose}>
       <Stack direction={'row'} gap={'1rem'} alignItems={'center'} marginBottom={'1.5rem'}>
         <IconApp picture={appIcon} alt={appName} size="large" domain={appUrl} />
         <Box overflow={'auto'}>
@@ -67,8 +75,8 @@ export const ModalNostrConnect = () => {
 
       <Stack gap={'1rem'}>
         <Typography variant="subtitle1">Choose account to connect to this app:</Typography>
-        <Keys keys={keys} />
-        <Typography variant="body1" color={'GrayText'}>
+        <Keys keys={keys} onKeyClick={connect} isLoading={isLoading} />
+        <Typography variant="subtitle2" color={'GrayText'} fontSize={'12px'}>
           Please check that app provided the correct name, address and icon to avoid confusion.
         </Typography>
       </Stack>
--- packages/client/src/components/Modal/ModalNostrConnect/components/Keys.tsx
+++ packages/client/src/components/Modal/ModalNostrConnect/components/Keys.tsx
@@ -5,12 +5,16 @@ import { ItemKey } from './ItemKey'
 
 type KeysProps = {
   keys: DbKey[]
+  onKeyClick: (npub: string) => void
+  isLoading: boolean
 }
 
-export const Keys: FC<KeysProps> = ({ keys }) => {
+export const Keys: FC<KeysProps> = ({ keys, onKeyClick, isLoading }) => {
   const handleKeyClick = (key: DbKey) => {
-    console.log(key)
+    if (isLoading) return
+    onKeyClick(key.npub)
   }
+
   return (
     <Box flex={1} overflow={'auto'} borderRadius={'8px'} maxHeight={'30rem'}>
       {keys.map((key) => (
