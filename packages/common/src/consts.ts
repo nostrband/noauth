fa59b1dad482c1235ee760d439b50200c9efd8bd
artur <brugeman.artur@gmail.com>
2024-10-17 16:42:29 +0200

    Fix broadcast relays

--- packages/common/src/consts.ts
+++ packages/common/src/consts.ts
@@ -3,9 +3,9 @@ export const KIND_RPC = 24133
 export const MIN_POW = 11
 export const MAX_POW = 17
 
-export const OUTBOX_RELAYS = ['wss://relay.nos.social', 'wss://nos.lol', 'wss://purplepag.es']
+export const OUTBOX_RELAYS = ['wss://relay.nos.social', 'wss://nos.lol', 'wss://purplepag.es', 'wss://relay.nostr.band']
 export const SEED_RELAYS = ['wss://relay.damus.io', 'wss://nostr.mom', 'wss://relay.primal.net']
-export const BROADCAST_RELAY = 'wss://relay.nos.social'
+export const BROADCAST_RELAYS = [...OUTBOX_RELAYS, ...SEED_RELAYS]
 
 export const REQ_TTL = 60000 // 1 min
 
