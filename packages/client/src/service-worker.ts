935a903efa49a945c339310c425bb7ae6ff8c95d
artur <brugeman.artur@gmail.com>
2024-10-22 16:27:28 +0200

    Testing removing precaching to improve sw load time

--- packages/client/src/service-worker.ts
+++ packages/client/src/service-worker.ts
@@ -23,7 +23,10 @@ clientsClaim()
 // Their URLs are injected into the manifest variable below.
 // This variable must be present somewhere in your service worker file,
 // even if you decide not to use precaching. See https://cra.link/PWA
-precacheAndRoute(self.__WB_MANIFEST)
+//precacheAndRoute(self.__WB_MANIFEST)
+
+// eslint-disable-next-line no-restricted-globals
+const ignored = self.__WB_MANIFEST;
 
 // Set up App Shell-style routing, so that all navigation requests
 // are fulfilled with your index.html shell. Learn more at
