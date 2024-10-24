56e1761d45282106f8cb02f1c7616ff154409a9f
Bekbolsun <bekbolsun.azamatov.1579.7@gmail.com>
2024-10-24 15:11:15 +0600

    trying to implement chrome extension

--- packages/client/config-overrides.js
+++ packages/client/config-overrides.js
@@ -1,6 +1,8 @@
 const path = require('path')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = function override(config) {
+  config.ignoreWarnings = [/Failed to parse source map/]
   const fallback = config.resolve.fallback || {}
   Object.assign(fallback, {
     http: require.resolve('stream-http'),
@@ -10,6 +12,27 @@ module.exports = function override(config) {
   config.resolve.fallback = fallback
   config.resolve.alias = {
     '@': path.resolve(__dirname, 'src'),
+  }
+
+  if (process.env.BUILD_TARGET === 'ext-chrome') {
+    config.entry = {
+      main: './src/ext/popup.ts',
+      background: './src/ext/background.ts',
+      content: './src/ext/content.ts',
+    }
+    config.output = {
+      ...config.output,
+      filename: 'static/js/[name].js',
+      chunkFilename: 'static/js/[name].js',
+      path: path.resolve(__dirname, 'ext-build'),
+    }
+    config.plugins.push(
+      new CopyPlugin({
+        patterns: [{ from: 'src/ext/manifest.json', to: 'manifest.json' }],
+      })
+    )
+  } else {
+    config.output.path = path.resolve(__dirname, 'build')
   }
 
   return config
