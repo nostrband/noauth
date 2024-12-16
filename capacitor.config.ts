4d994b4985e8a3f678dbe85f3945a7cd7794626a
Bekbolsun <bekbolsun.azamatov.1579.7@gmail.com>
2024-12-16 14:56:48 +0600

    test build

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nostr.band',
+  appName: '@noauth/ios',
+  webDir: './packages/client/build'
+};
+
+export default config;
