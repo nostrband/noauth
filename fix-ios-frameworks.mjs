304c9836671f8ffca5a90caa8ac92cfb0a6ff06d
Bekbolsun <bekbolsun.azamatov.1579.7@gmail.com>
2025-01-21 17:39:13 +0600

    fix appflow build process

---
+++ fix-ios-frameworks.mjs
@@ -0,0 +1,16 @@
+import { execSync } from 'child_process'
+import fs from 'fs'
+
+const appName = 'App'
+const filePath = `ios/App/Pods/Target Support Files/Pods-${appName}/Pods-${appName}-frameworks.sh`
+
+try {
+  if (fs.existsSync(filePath)) {
+    execSync(`sed -i '' 's|source="$(readlink "\${source}")"|source="$(readlink -f "\${source}")"|' "${filePath}"`)
+    console.log(`Updated ${filePath}`)
+  } else {
+    console.log(`File not found: ${filePath}`)
+  }
+} catch (error) {
+  console.error('Error executing script:', error)
+}
