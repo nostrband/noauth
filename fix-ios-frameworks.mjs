9283930ad829cd613f0977c2582cb99a4af62905
Bekbolsun <bekbolsun.azamatov.1579.7@gmail.com>
2025-01-21 16:44:37 +0600

    test build #4.3

--- fix-ios-frameworks.mjs
+++ fix-ios-frameworks.mjs
@@ -4,9 +4,13 @@ import { existsSync } from 'fs'
 const appName = 'App'
 const filePath = `ios/App/Pods/Target Support Files/Pods-${appName}/Pods-${appName}-frameworks.sh`
 
-if (existsSync(filePath)) {
-  execSync(`sed -i '' 's|source="$(readlink "\${source}")"|source="$(readlink -f "\${source}")"|' ${filePath}`)
-  console.log(`Updated ${filePath}`)
-} else {
-  console.log(`File not found: ${filePath}`)
+try {
+  if (fs.existsSync(filePath)) {
+    execSync(`sed -i '' 's|source="$(readlink "\${source}")"|source="$(readlink -f "\${source}")"|' "${filePath}"`)
+    console.log(`Updated ${filePath}`)
+  } else {
+    console.log(`File not found: ${filePath}`)
+  }
+} catch (error) {
+  console.error('Error executing script:', error)
 }
