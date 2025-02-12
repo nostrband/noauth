6945d303b88dadb12b4ef5b7b6b9ad020a7667ce
Bekbolsun <bekbolsun.azamatov.1579.7@gmail.com>
2025-02-12 21:35:08 +0600

    add android & test build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
-  appId: 'app.nsec.native',
+  appId: 'nostr.nsec.app',
   appName: 'Nsec.app',
   webDir: './packages/client/build',
   ios: {
