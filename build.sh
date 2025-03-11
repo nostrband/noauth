cffaa22d8c7c28e830becfce5a21ad28fca76e7c
artur <brugeman.artur@gmail.com>
2025-03-11 14:49:31 +0100

    Add Dockerfile param to build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+FILE=$1
 NAME=noauth_server
 SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
 echo "Commit timestamp" $SOURCE_DATE_EPOCH
@@ -16,16 +17,9 @@ docker \
     --frontend dockerfile.v0 \
     --opt platform=linux/amd64 \
     --opt build-arg:SOURCE_DATE_EPOCH=${SOURCE_DATE_EPOCH} \
-    --opt filename=./Dockerfile-enclave \
+    --opt filename=${FILE} \
     --local dockerfile=. \
     --local context=. \
-    --output type=docker,name=${NAME},dest=${NAME}.tar,rewrite-timestamp=true \
-
-#    --progress=plain \
-#    --rm \
-
-#  buildctl 
-#--output type=oci,dest=dest.tar,rewrite-timestamp=true \
-#  --output type=image,name=example.com/foo:$SOURCE_DATE_EPOCH,buildinfo=false,push=$PUSH,rewrite-timestamp=true \
+    --output type=docker,name=${NAME},dest=${NAME}.tar,buildinfo=false,rewrite-timestamp=true \
   
 
